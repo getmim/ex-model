5eef6752ad88843d29a0680f1b26efdbdd8b3600
Iqbal Fauzi <iqbalfawz@gmail.com>
2023-06-09 12:07:39 +0700

    Add model relation

--- modules/ex-model/config.php
+++ modules/ex-model/config.php
@@ -2,7 +2,7 @@
 
 return [
     '__name' => 'ex-model',
-    '__version' => '0.0.1',
+    '__version' => '0.0.2',
     '__git' => 'git@github.com:getmim/ex-model.git',
     '__license' => 'MIT',
     '__author' => [
--- modules/ex-model/model/ModelChildren.php
+++ modules/ex-model/model/ModelChildren.php
@@ -12,7 +12,12 @@ class ModelChildren extends \Mim\Model
 
     protected static $table = 'model_children';
 
-    protected static $chains = [];
+    protected static $chains = [
+        'parent' => [
+            'model' => 'ExModel\\Model\\ModelParent',
+            'field' => 'id'
+        ]
+    ];
 
-    protected static $q = [];
+    protected static $q = ['type_char', 'type_varchar'];
 }
